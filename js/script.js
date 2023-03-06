58fb183e32f89b1758a747e68fb47fb5d1992ebb
manuelnones <manuelnones99@yahoo.com>
2023-03-06 18:22:05 +0100

    correct exercise + add function array numbers

--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
+/*
+    - creare un array di 16 numeri casuali
+    - se il livello è facile
+        o inserire l'array di numeri tra il numero 1 e 100
+    - altrimenti se il livello è medio
+        o inserire l'array di numeri tra il numero 1 e 81
+    - altrimenti se il livello è difficile
+        o inserire l'array di numeri tra il numero 1 e 49
+*/
+
 const btnEl = document.getElementById(`btn`);
 const mainContainerEl = document.getElementById(`main-container`);
 const levelEl = document.getElementById(`level`);
+const bombElements = arrayNumberGenerator(16);
+console.log(bombElements)
 
 btnEl.addEventListener(`click`, function () {
     mainContainerEl.innerHTML = ``;
 
     if (levelEl.value == 1) {
-        createGrid (100, 10);
+        createGrid(100, 10);
 
     } else if (levelEl.value == 2) {
-        createGrid (81, 9);
+        createGrid(81, 9);
 
     } else if (levelEl.value == 3) {
-        createGrid (49, 7);
+        createGrid(49, 7);
 
-    }
+    };
 
 });
 
 function createSquare(squareText, columnNumber) {
-    let newSquareEl = document.createElement(`div`);
-    newSquareEl.classList.add(`square`);
-    newSquareEl.style.width = `calc(100% / ${columnNumber})`;
-    newSquareEl.style.height = `calc(100% / ${columnNumber})`;
-    newSquareEl.textContent = squareText;
+    let squareEl = document.createElement(`div`);
+    squareEl.classList.add(`square`);
+    squareEl.style.width = `calc(100% / ${columnNumber})`;
+    squareEl.style.height = `calc(100% / ${columnNumber})`;
+    squareEl.textContent = squareText;
 
-    return newSquareEl;
+    squareEl.addEventListener(`click`, function () {
+        squareEl.classList.toggle(`active`);
+    });
+
+    return squareEl;
 };
 
 function createGrid(squareNumber, columnNumber) {
@@ -34,13 +50,29 @@ function createGrid(squareNumber, columnNumber) {
     mainContainerEl.append(newGridEl);
 
     for (let i = 1; i <= squareNumber; i++) {
-        let squareEl = createSquare (i, columnNumber);
+        squareEl = createSquare(i, columnNumber);
         newGridEl.append(squareEl);
 
-        squareEl.addEventListener(`click`, function () {
-            squareEl.classList.toggle(`active`);
-        });
-        
-    }
+    };
+
     return squareEl;
+};
+
+function randomNumberGenerator(lastNumber) {
+    let randomNumber = Math.floor(Math.random() * lastNumber + 1);
+    
+    return randomNumber;
+};
+
+function arrayNumberGenerator() {
+    let arrayNumbers = [
+        randomNumberGenerator(16), randomNumberGenerator(16), randomNumberGenerator(16),
+        randomNumberGenerator(16), randomNumberGenerator(16), randomNumberGenerator(16),
+        randomNumberGenerator(16), randomNumberGenerator(16), randomNumberGenerator(16),
+        randomNumberGenerator(16), randomNumberGenerator(16), randomNumberGenerator(16),
+        randomNumberGenerator(16), randomNumberGenerator(16), randomNumberGenerator(16),
+        randomNumberGenerator(16)
+    ];
+
+    return arrayNumbers;
 };
